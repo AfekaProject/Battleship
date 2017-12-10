e0904ef2e5d616b883913b5e1477a1543d177a43
GuyCo <co.guycohen@gmail.com>
2017-12-10 14:39:25 +0200

    Merge branch 'master' of C:\Users\Guy\AndroidStudioProjects\Battleship with conflicts.

--- app/src/main/java/afeka/battleship/View/TileAdapter.java
+++ app/src/main/java/afeka/battleship/View/TileAdapter.java
@@ -5,6 +5,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import afeka.battleship.Model.Board;
+import afeka.battleship.Model.Tile;
+import afeka.battleship.R;
 import afeka.battleship.logic.Game;
 
 public class TileAdapter extends BaseAdapter {
@@ -47,13 +49,37 @@ public class TileAdapter extends BaseAdapter {
         else{
             tileView = (TileView) view;
         }
+        Tile.Status status = mBoard.getTile(i).getStatus();
 
-        String text = mBoard.getTile(i).toString();
+        switch (status) {
+            case NONE:
+                tileView.img.setImageResource(R.drawable.img_empty);
+                break;
+            case PLACED:
+                if (playerToView.equals(Game.Players.PLAYER))
+                    tileView.img.setImageResource(R.drawable.img_empty);
+                else
+                    tileView.img.setImageResource(R.drawable.img_hit);
+                break;
+            case HIT:
+                tileView.img.setImageResource(R.drawable.img_hit);
+                break;
+            case MISS:
+                tileView.img.setImageResource(R.drawable.img_miss);
+                break;
+            case DROWNED:
+                tileView.img.setImageResource(R.drawable.img_dead_fish);
+                break;
+            default:
+                tileView.img.setImageResource(R.drawable.img_empty);
+                break;
+        }
+       // String text = mBoard.getTile(i).toString();
 
         //disable this line to view enemy ship for debug
   //      text = hideShips(text);
 
-        tileView.text.setText(text);
+        //tileView.text.setText(text);
 
             return tileView;
     }
--- app/src/main/java/afeka/battleship/View/TileView.java
+++ app/src/main/java/afeka/battleship/View/TileView.java
@@ -1,24 +1,34 @@
 package afeka.battleship.View;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.graphics.Color;
+import android.view.Gravity;
 import afeka.battleship.R;
 
 public class TileView extends FrameLayout {
 
     TextView text;
+    ImageView img;
 
     public TileView(Context context) {
         super(context);
         //put here setting for how to view the tile in grid
+        img = new ImageView(context);
+        LayoutParams layoutParams = new LinearLayout.LayoutParams(90,80);
+        img.setLayoutParams(layoutParams);
+        img.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        img.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        addView(img);
+
+        /*
         text = new TextView(context);
-
-
         LayoutParams layoutParams = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         text.setLayoutParams(layoutParams);
         text.setTextAlignment(TEXT_ALIGNMENT_CENTER);
